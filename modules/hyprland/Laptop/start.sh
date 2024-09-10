43a9e142c4fc0d72fbc0c1e14dbf3818ebf284f6
Tom <tom.sander243@gmail.com>
2024-09-10 14:07:27 +0200

    new waybar and background

--- modules/hyprland/Laptop/start.sh
+++ modules/hyprland/Laptop/start.sh
@@ -4,9 +4,7 @@ swww init &
 
 sleep 0.1 &&
 
-swww img -o DP-1 ~/main-background-image.jpg &
-swww img -o DP-2 ~/main-background-image.jpg &
-swww img -o HDMI-A-1 ~/second-background-image.jpeg &
+swww img -o eDP-1 ~/main-background-image.jpg &
 
 nm-applet --indicator &
 waybar &
